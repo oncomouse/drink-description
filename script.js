bd016f46fc017fa0afef0af51c589af962a7857d
onocmouse <onocmouse@gmail.com>
2016-07-24 14:18:02 -0500

    Don't need the test script anymore.

--- script.js
+++
@@ -1,4 +0,0 @@
-import drinkDescription from './index';
-import _ from 'lodash';
-
-_.times(6, () => console.log(drinkDescription(['Blended Aged Rum', 'Black Blended Rum', 'Blended Lightly Aged Rum', 'Lime', 'Grenadine', 'Demerara Syrup', 'Blackberry Brandy', 'Angostura Bitters'])));

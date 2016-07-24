6eabaa7e0645d9560ba27ed31d98f22bf56eaf28
onocmouse <onocmouse@gmail.com>
2016-07-24 14:11:30 -0500

    Removed an unused dependency

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import drinkDescription from './index';
-import { permutations } from './lib/permutations';
 import _ from 'lodash';
 
-_.times(6, () => console.log(drinkDescription(['Blended Aged Rum', 'Black Blended Rum', 'Blended Lightly Aged Rum', 'Lime', 'Grenadine', 'Demerara Syrup', 'Blackberry Brandy', 'Angostura Bitters'])))
+_.times(6, () => console.log(drinkDescription(['Blended Aged Rum', 'Black Blended Rum', 'Blended Lightly Aged Rum', 'Lime', 'Grenadine', 'Demerara Syrup', 'Blackberry Brandy', 'Angostura Bitters'])));
